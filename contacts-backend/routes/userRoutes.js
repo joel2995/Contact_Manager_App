b12d3418610d1a56956e97d5c62698a6df7147ed
Joel Sundarsingh A <joelsundarsingh2005@gmail.com>
2025-04-17 22:04:30 +0530

    Users registration

---
+++ contacts-backend/routes/userRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", (req , res) =>{
+    res.json({message : "Register the user"});
+
+});
+
+
+router.post("/login" , (req , res) =>{
+    res.json({message : "Login the user"});
+});
+
+
+router.post("/current" , (req , res) => {
+    res.json({message : "Current user information"});
+});
