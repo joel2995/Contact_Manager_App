208651334e55301a8214669fda138fc9516acaae
joel2995 <joelsundarsingh2005@gmail.com>
2025-05-16 22:42:34 +0530

    CR done

--- contacts-backend/controllers/contactController.js
+++ contacts-backend/controllers/contactController.js
@@ -2,11 +2,11 @@ const asyncHandler = require("express-async-handler");
 const Contact = require ("../models/contactModel");
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access public 
+//@access private 
 
 
 const getContacts = asyncHandler(async(req , res) => {
-    const contacts =await Contact.find();
+    const contacts =await Contact.find({user_id : req.user.id});
 
     res.status(200).json(contacts);
 });
@@ -14,7 +14,7 @@ const getContacts = asyncHandler(async(req , res) => {
 
 //@desc Create all contacts
 //@route POST /api/contacts
-//@access public 
+//@access private 
 
 
 const createContact =  asyncHandler(async(req , res) => {
@@ -25,9 +25,11 @@ const createContact =  asyncHandler(async(req , res) => {
         throw new Error("All fields are Mandatory");
     }
     const contact = await Contact.create({
-        name , 
-        email , 
-        phone , 
+        name, 
+        email, 
+        phone,
+        user_id: req.user.id,
+
     });
 
     res.status(201).json(contact);
@@ -37,7 +39,7 @@ const createContact =  asyncHandler(async(req , res) => {
 
 //@desc GET contact
 //@route GET /api/contacts/:id
-//@access public 
+//@access private 
 
 const getContact = asyncHandler(async(req , res) =>{
     const contact = await Contact.findById(req.params.id);
@@ -51,7 +53,7 @@ const getContact = asyncHandler(async(req , res) =>{
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
-//@access public 
+//@access private
 
 
 const updateContact = asyncHandler(async(req , res) =>{
@@ -70,7 +72,7 @@ const updateContact = asyncHandler(async(req , res) =>{
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
-//@access public 
+//@access private 
 
 const deleteContact = asyncHandler(async(req , res) =>{
     const contact = await Contact.findByIdAndDelete(req.params.id);
